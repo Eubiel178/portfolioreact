bb30d74e8ab53ce144cb29e9ed44f5ffcdc50081
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-06-28 21:33:52 -0500

    Add files via upload

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,20 +5,20 @@ function NavBar() {
     return(
         <ul className={Styles.navbar}>
 
-            <li className={Styles.navItem}>
-                <Link className={Styles.navItem} to="/aboutme">Sobre Mim</Link>
+            <li className={Styles.li}>
+                <Link className={Styles.navItemLink} to="/aboutme">Sobre Mim</Link>
             </li>
 
-            <li className={Styles.navItem}>
-                <Link className={Styles.navItem}  to="/formation">Formação</Link>
+            <li className={Styles.li}>
+                <Link className={Styles.navItemLink}  to="/formation">Formação</Link>
             </li>
 
-            <li className={Styles.navItem}>
-                <Link className={Styles.navItem} to="/experience">Experiência</Link>
+            <li className={Styles.li}>
+                <Link className={Styles.navItemLink} to="/experience">Experiência</Link>
             </li>
 
-            <li className={Styles.navItem}>
-                <Link to="/">Contato</Link>
+            <li className={Styles.li}>
+                <Link className={Styles.navItemLink} to="/">Contato</Link>
             </li>
         </ul>
     )
