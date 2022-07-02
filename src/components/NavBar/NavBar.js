dc2aa95337d5124c87fb8d8f4573e6c83ac925a6
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-07-02 18:20:04 -0500

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,24 +3,26 @@ import Styles from "./NavBar.module.css"
 
 function NavBar() {
     return(
-        <ul className={Styles.navbar}>
+        <nav>
+            <ul className={Styles.navbar}>
 
-            <li className={Styles.li}>
-                <Link className={Styles.navItemLink} to="/aboutme">Sobre Mim</Link>
-            </li>
+                <li className={Styles.li}>
+                    <Link className={Styles.navItemLink} to="/aboutme">Sobre Mim</Link>
+                </li>
 
-            <li className={Styles.li}>
-                <Link className={Styles.navItemLink}  to="/formation">Formação</Link>
-            </li>
+                <li className={Styles.li}>
+                    <Link className={Styles.navItemLink}  to="/formation">Formação</Link>
+                </li>
 
-            <li className={Styles.li}>
-                <Link className={Styles.navItemLink} to="/experience">Experiência</Link>
-            </li>
+                <li className={Styles.li}>
+                    <Link className={Styles.navItemLink} to="/experience">Experiência</Link>
+                </li>
 
-            <li className={Styles.li}>
-                <Link className={Styles.navItemLink} to="/">Contato</Link>
-            </li>
-        </ul>
+                <li className={Styles.li}>
+                    <Link className={Styles.navItemLink} to="/">Contato</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
